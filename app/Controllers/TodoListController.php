04b6fa8c088071f8c060cb79166ea7d39b519ba3
MnDAlex <alexvok12@gmail.com>
2020-12-08 17:35:17 +0100

    List todos

--- app/Controllers/TodoListController.php
+++ app/Controllers/TodoListController.php
@@ -2,8 +2,16 @@
 
 class TodoListController extends Controller
 {
-    public function index()
+    public function index(Base $f3): void
     {
-        echo "Hello world!";
+        $f3->set('todos', $f3->DB->exec(
+                'SELECT * FROM todos ' .
+                'WHERE user_id=:user_id',
+            [
+                ':user_id' => 1
+            ]
+        ));
+
+        $f3->set('include', 'todo-list/index.htm');
     }
 }
