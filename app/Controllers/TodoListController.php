490c8c64a2f6858a4355f4dd3e6310b0557d4012
MnDAlex <alexvok12@gmail.com>
2020-12-09 10:42:05 +0100

    Add authorization for TodoListController

--- app/Controllers/TodoListController.php
+++ app/Controllers/TodoListController.php
@@ -7,39 +7,46 @@ class TodoListController
     public function __construct(Base $f3)
     {
         $this->todo = new DB\SQL\Mapper($f3->DB, 'todos');
-        
     }
 
     public function index(Base $f3): void
     {
-        $page = $_GET['page'] ?: 1;
+        if ($f3->exists('SESSION.userId')) {
+            $page = $_GET['page'] ?: 1;
 
-        $take = 10;
+            $take = 10;
 
-        $todos = $this->todo->paginate($page - 1, $take, [
-            'user_id=?', 1
-        ], [
-            'order' => 'id DESC'
-        ]);
+            $todos = $this->todo->paginate($page - 1, $take, [
+                'user_id=?', $f3->get('SESSION.userId')
+            ], [
+                'order' => 'id DESC'
+            ]);
 
-        $f3->set('todos', $todos);
+            $f3->set('todos', $todos);
 
-        $f3->set('extraScripts', [
-            $f3->BASE . '/js/todo-list/index.js'
-        ]);
+            $f3->set('extraScripts', [
+                $f3->BASE . '/js/todo-list/index.js'
+            ]);
 
-        $f3->set('view', 'todo-list/index.htm');
+            $f3->set('view', 'todo-list/index.htm');
+        } else {
+            $f3->set('view', 'unauthorized.htm');
+        }
 
         echo Template::instance()->render('layout.htm');
     }
 
     public function store(Base $f3)
     {
+        if (!$f3->exists('SESSION.userId')) {
+            $f3->error(401, 'Unauthorized');
+        }
+
         $this->todo->todo = $_POST['todo'];
 
         $this->todo->deadline = $_POST['deadline'] ?: null;
 
-        $this->todo->user_id = 1;
+        $this->todo->user_id = $f3->get('SESSION.userId');
 
         $this->todo->save();
 
@@ -48,6 +55,10 @@ class TodoListController
 
     public function toggleCompleted(Base $f3)
     {
+        if (!$f3->exists('SESSION.userId')) {
+            $f3->error(401, 'Unauthorized');
+        }
+
         parse_str(file_get_contents("php://input"), $request);
 
         $f3->DB->exec(
