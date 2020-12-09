38337eafb6183ab27865b84a42517d7e7f8b12b6
MnDAlex <alexvok12@gmail.com>
2020-12-09 12:38:01 +0100

    Implement registration

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -23,11 +23,46 @@ class AuthController
             'pw' => 'password'
         ]);
 
-        if($auth->login($_POST['username'], $_POST['password'])){
+        if ($auth->login($_POST['username'], $_POST['password'])) {
             $f3->set('SESSION.userId', $this->user->id);
         }
 
         $f3->reroute('@todo_list', true);
+    }
+
+    public function registerView(Base $f3)
+    {
+        $f3->set('view', 'register.htm');
+
+        echo Template::instance()->render('layout.htm');
+    }
+
+    public function register(Base $f3)
+    {
+        $validator = new Validator($_POST, [
+            'username' => ['required', 'min:4', 'max:45', 'unique:users,username'],
+            'password' => ['required', 'min:6', 'confirmed'],
+        ]);
+
+        if (!$validator->validate()) {
+            $f3->set('errors', $validator->errors());
+
+            $f3->set('view', 'register.htm');
+
+            $f3->set('old', $_POST);
+
+            echo Template::instance()->render('layout.htm');
+        } else {
+            $this->user->username = $_POST['username'];
+
+            $this->user->password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+            $this->user->save();
+
+            $f3->set('SESSION.userId', $this->user->id);
+
+            $f3->reroute('@todo_list', true);
+        }
     }
 
     public function logout(Base $f3)
