70e3ef527687988e3e784a1c89d9fae1a33886bf
MnDAlex <alexvok12@gmail.com>
2020-12-09 13:30:53 +0100

    Connect to the db via env variables

--- config/services.php
+++ config/services.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = new DB\SQL(
-    'mysql:host=localhost;post=3306;dbname=f3-todo-list;charset=utf8',
+    'mysql:host='.$f3->DB_HOST.';post='.$f3->DB_PORT.';dbname='.$f3->DB_TABLE.';charset=utf8',
     $f3->DB_USERNAME,
     $f3->DB_PASSWORD,
     [
