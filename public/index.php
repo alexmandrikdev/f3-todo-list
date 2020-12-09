25f10e88fbdc34dec99df3ac446f96cab88a2e6c
MnDAlex <alexvok12@gmail.com>
2020-12-09 13:09:25 +0100

    Add .env

--- public/index.php
+++ public/index.php
@@ -4,9 +4,12 @@ require_once(__DIR__.'/../vendor/autoload.php');
 
 require_once(__DIR__.'/../app/helpers.php');
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+
 $f3 = Base::instance();
 
-$f3->config(__DIR__.'/../config/config.ini');
+$f3->config(__DIR__.'/../config/config.ini', true);
 
 $f3->config(__DIR__.'/../config/routes.ini');
 
