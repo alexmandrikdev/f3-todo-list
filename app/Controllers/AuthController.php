343bc237f08ca239918a444bfab0166f4c6b6d82
MnDAlex <alexvok12@gmail.com>
2020-12-09 10:26:42 +0100

    Implement login

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+class AuthController
+{
+    private $user;
+
+    public function __construct(Base $f3)
+    {
+        $this->user = new DB\SQL\Mapper($f3->DB, 'users');
+    }
+
+    public function loginView(Base $f3)
+    {
+        $f3->set('view', 'login.htm');
+
+        echo Template::instance()->render('layout.htm');
+    }
+
+    public function login(Base $f3)
+    {
+        $auth = new Auth($this->user, [
+            'id' => 'username',
+            'pw' => 'password'
+        ]);
+
+        if($auth->login($_POST['username'], $_POST['password'])){
+            $f3->set('SESSION.userId', $this->user->id);
+        }
+
+        $f3->reroute('@todo_list', true);
+    }
+}
