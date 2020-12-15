83dcbf4cbdf44a633b94c230aa2b5a73b7dfb097
MnDAlex <alexvok12@gmail.com>
2020-12-15 10:33:33 +0100

    Create getForIndex Todo function

--- app/Controllers/TodoListController.php
+++ app/Controllers/TodoListController.php
@@ -6,39 +6,13 @@ class TodoListController
 
     public function __construct(Base $f3)
     {
-        $this->todo = new DB\SQL\Mapper($f3->DB, 'todos');
+        $this->todo = new Todo();
     }
 
     public function index(Base $f3): void
     {
         if ($f3->exists('SESSION.userId')) {
-            $page = $_GET['page'] ?: 1;
-
-            $take = 10;
-
-            $order = $_GET['sort'] ?: '';
-
-            $filter['0'] = 'user_id=:user_id';
-
-            $filter[':user_id'] = $f3->get('SESSION.userId');
-
-            if ($_GET['hide_completed'] === 'on') {
-                $filter['0'] .= ' AND completed_at is null';
-            }
-
-            if ($_GET['hide_no_deadline'] === 'on') {
-                $filter['0'] .= ' AND deadline is not null';
-            }
-
-            if ($_GET['todo_filter']) {
-                $filter['0'] .= ' AND todo LIKE :todo_filter';
-
-                $filter[':todo_filter'] = "%" . $_GET['todo_filter'] . "%";
-            }
-
-            $todos = $this->todo->paginate($page - 1, $take, $filter, [
-                'order' => $this->determineOrder($order)
-            ]);
+            $todos = $this->todo->getForIndex();
 
             $f3->set('todos', $todos);
 
@@ -178,41 +152,6 @@ class TodoListController
             $this->todo->update();
         } else {
             $f3->error(422, json_encode($validator->errors()));
-        }
-    }
-
-    /**
-     * Determine the order based on the $order param
-     * 
-     * @param string $order 
-     * @return string 
-     */
-    private function determineOrder(string $order): string
-    {
-        switch ($order) {
-            case 'name':
-                return 'todo';
-                break;
-
-            case 'name_desc':
-                return 'todo DESC';
-                break;
-
-            case 'deadline':
-                return 'deadline';
-                break;
-
-            case 'deadline_desc':
-                return 'deadline DESC';
-                break;
-
-            case 'add_date':
-                return 'id';
-                break;
-
-            default:
-                return 'id DESC';
-                break;
         }
     }
 }
--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -11,4 +11,69 @@ class Todo extends DB\SQL\Mapper
         parent::__construct($this->f3->DB, 'todos');
     }
 
+    function getForIndex()
+    {
+        $page = $_GET['page'] ?: 1;
+
+        $take = 10;
+
+        $order = $_GET['sort'] ?: '';
+
+        $filter['0'] = 'user_id=:user_id';
+
+        $filter[':user_id'] = $this->f3->get('SESSION.userId');
+
+        if ($_GET['hide_completed'] === 'on') {
+            $filter['0'] .= ' AND completed_at is null';
+        }
+
+        if ($_GET['hide_no_deadline'] === 'on') {
+            $filter['0'] .= ' AND deadline is not null';
+        }
+
+        if ($_GET['todo_filter']) {
+            $filter['0'] .= ' AND todo LIKE :todo_filter';
+
+            $filter[':todo_filter'] = "%" . $_GET['todo_filter'] . "%";
+        }
+
+        return $this->paginate($page - 1, $take, $filter, [
+            'order' => $this->determineOrder($order)
+        ]);
+    }
+
+    /**
+     * Determine the order based on the $order param
+     * 
+     * @param string $order 
+     * @return string 
+     */
+    private function determineOrder(string $order): string
+    {
+        switch ($order) {
+            case 'name':
+                return 'todo';
+                break;
+
+            case 'name_desc':
+                return 'todo DESC';
+                break;
+
+            case 'deadline':
+                return 'deadline';
+                break;
+
+            case 'deadline_desc':
+                return 'deadline DESC';
+                break;
+
+            case 'add_date':
+                return 'id';
+                break;
+
+            default:
+                return 'id DESC';
+                break;
+        }
+    }
 }
